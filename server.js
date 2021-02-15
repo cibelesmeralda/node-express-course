4f9298684ac1518b77785501ddac76f2d709e1c0
Cibeles-Callejas <Cibeles.Callejas@coxautoinc.com>
2021-02-15 18:53:47 -0400

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
 
 const mockUserData=[
     {name:'Cibe'},
     {name:'MAti'}
 ]
+
 app.get('/users/:id', function(req,res){
     console.log(req.params.id)
     res.json({
@@ -12,6 +15,27 @@ app.get('/users/:id', function(req,res){
         message: 'got one user',
         users: req.params.id
     })
+})
+
+app.post('/login',function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+    
+    const mockUsername = "cibe"
+    const mockPassword = "pass"
+
+    if(username == mockUsername && password == mockPassword){
+        res.json({
+            success: true,
+            message: 'passwordn and username match',
+            token: 'encripted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
 })
 
 app.listen(8000, function(){
